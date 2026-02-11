1a4f4c442e72b3484f31287c37343316bf74c432
Sree&Ash-Mon <sreeash1235@gmail.com>
2026-02-11 20:28:07 +0530

    Create random_forest.py

---
+++ 08_Random-Forest/random_forest.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+df = pd.read_csv('iris.csv')
+
+X = df.drop('class', axis=1)
+y = df['class']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.3,
+    random_state=0
+)
+
+model = RandomForestClassifier(
+    n_estimators=50,
+    random_state=0
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred) * 100)
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
